032b4f15fc2a9404621db62da0b70409c0dd92a8
David <ditmarcastro@gmail.com>
2019-08-20 21:29:30 -0400

    new version

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,13 @@ router.get('/', function(req, res, next) {
     res.render('index', {info: docs[0].contentdom});
   });
 });
+router.get('/programa', function(req, res, next) {
+  ARTICULE.find({}).exec((err, docs) => {
+    console.log(docs);
+    var a = 0;
+    res.render('programa', {info: docs[1].contentdom});
+  });
+  //console.log("In here man ");
+});
 
 module.exports = router;
